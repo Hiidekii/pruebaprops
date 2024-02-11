2d6b10517352152c8ee300bbf647e590ce60d623
HidekiS <20212604@aloe.ulima.edu.pe>
2024-02-11 16:56:04 -0500

    variables de estado

--- src/Imagen.jsx
+++ src/Imagen.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react"
+
 const Imagen = (props) => { //ponemos props para indicar que pasamos argumentos
+    //Variable de estado
+    const [color /*nombre var*/, setColor /*funcion q modifica*/] = useState("#FFFFFF")
+
     const estiloCard = {
         width: "300px",
-        marginBottom: "20px"
+        marginBottom: "20px",
+        backgroundColor: color
     }
 
-    return <div className="card" style={estiloCard}>
+    const cardOnClick = () => {
+        //funcion que modifica el color del card
+        if (color == "#FFFFFF") setColor("#FF10FF") //Modifica la variable de estado (regenera componente)
+        else setColor("#FFFFFF")
+    }
+
+    return <div className="card" style={estiloCard} onClick={cardOnClick}>
         <img //le pasamos por props los argumentos
             src={props.url} className="card-img-top" />
         <div className="card-body">
